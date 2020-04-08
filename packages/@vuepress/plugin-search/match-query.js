b4d1bbfed3c46a21d40e9a3c3aa7c660601af279
Restyled.io <commits@restyled.io>
2020-04-08 08:25:56 +0000

    Restyled by clang-format

--- packages/@vuepress/plugin-search/match-query.js
+++ packages/@vuepress/plugin-search/match-query.js
@@ -21,30 +21,27 @@ const matchTest = (query, domain) => {
   // eslint-disable-next-line no-control-regex
   const nonASCIIRegExp = new RegExp('[^\x00-\x7F]')
 
-  const words = query
-    .split(/\s+/g)
-    .map(str => str.trim())
-    .filter(str => !!str)
+  const words = query.split(/\s+/g).map(str => str.trim()).filter(str => !!str)
 
   if (!nonASCIIRegExp.test(query)) {
     // if the query only has ASCII chars, treat as English
     const hasTrailingSpace = query.endsWith(' ')
-    const searchRegex = new RegExp(
-      words
-      .map((word, index) => {
-        if (words.length === index + 1 && !hasTrailingSpace) {
-          // The last word - ok with the word being "startswith"-like
-          return `(?=.*\\b${escapeRegExp(word)})`
-        } else {
-          // Not the last word - expect the whole word exactly
-          return `(?=.*\\b${escapeRegExp(word)}\\b)`
-        }
-      })
-      .join('') + '.+',
-      'gi'
-    )
+    const searchRegex =
+        new RegExp(words.map((word, index) => {
+                          if (words.length === index + 1 && !hasTrailingSpace) {
+                            // The last word - ok with the word being
+                            // "startswith"-like
+                            return `(?=.*\\b${escapeRegExp(word)})`
+                          } else {
+                            // Not the last word - expect the whole word exactly
+                            return `(?=.*\\b${escapeRegExp(word)}\\b)`
+                          }
+                        }).join('') +
+                       '.+',
+                   'gi')
     return searchRegex.test(domain)
-  } else {
+  }
+  else {
     // if the query has non-ASCII chars, treat as other languages
     return words.some(word => domain.toLowerCase().indexOf(word) > -1)
   }
